d4e2ebd8e7b7b74c5c3b1d616092edbd326c4abc
Pradeep Binwal <Pradeepbinwal1@gmail.com>
2024-07-18 00:19:28 +0530

    swaped-10

--- src/swapListElement/swap.jsx
+++ src/swapListElement/swap.jsx
@@ -1,24 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 
 const Swap = () => {
 
-    const list1=[{title:'list 1',checked:false},
+    const [list1 , setList1]=useState([{title:'list 1',checked:false},
         {title:'list 2',checked:false},
         {title:'list 3',checked:false}
-    ]
+    ])
 
-    const list2=[{title:'list a',checked:false},
+    const [list2,setList2]= useState([{title:'list a',checked:false},
         {title:'list b',checked:false},
-        {title:'list c',checked:false}
-    ]
+        {title:'list c',checked:false}])
 
 
     const handleCheck =(ind)=>{
-        const updated= {...list1};
-        // updated[ind].checked=!updated[ind].checked;
+        // console.log(ind)
+        const updated= [...list1]; 
+        updated[ind].checked=!updated[ind].checked;
         console.log(updated);
-        console.log(ind);
+        setList1(updated);
+        // console.log(ind);
         
+    }
+    const handleSwap = ()=>{
+        // console.log("button click")
+        const updated1= [...list1];
+        const updated2= [...list2];
+
+        updated1.forEach((item,ind)=>{
+            if(item.checked){
+                const temp= item.title;
+                item.title=updated2[ind].title;
+                updated2[ind].title=temp;
+                item.checked=false;
+                // console.log(item.checked)
+            }
+            
+        })
+
+        setList1(updated1);
+        setList2(updated2);
+        // console.log(updated1);
     }
 
   return (
@@ -27,28 +48,30 @@ const Swap = () => {
    <br/>
    
    <ol>
-    {
-        list1.map((el,ind)=>{
-            return <>
-            <li key={ind}>
-                <input type="checkbox" value={el.checked} onClick={(ind)=>handleCheck(ind)}/>
-                {el.title}</li>
-            </>
-        })
-    }
+   {list1.map((el, ind) => (
+                    <li key={ind}>
+                        <input
+                            type="checkbox"
+                            checked={el.checked}
+                            onChange={() => handleCheck(ind)}
+                        />
+                        {el.title}
+                    </li>
+                ))}
     
    </ol> 
 
    <ul>
    {
         list2.map((el,ind)=>{
-            return <>
-            <li>{el.title}</li>
-            </>
+            return <li key={ind}>{el.title}</li>
+           
         })
     }
    </ul> 
-     
+     <button onClick= {handleSwap}>
+        Swap Element
+     </button>
    </>
   )
 }
